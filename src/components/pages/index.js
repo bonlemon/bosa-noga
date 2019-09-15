877598ac45da2437a158aa2fc2b937758c586ec3
AVILOV Dmitry <ruaald6@n-msk-macald6.raiffeisen.ru>
2019-09-15 19:46:17 +0300

    add structure of components

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,6 +1,6 @@
-import Main from 'Main';
-import Catalog from 'Catalog';
-import About from 'About';
-import NotFound from 'NotFound';
+import Main from './Main';
+import Catalog from './Catalog';
+import About from './About';
+import NotFound from './NotFound';
 
 export { Main, Catalog, About, NotFound };
