9cbd623b9039671295b2fdac06e8369cc8e42850
AVILOV Dmitry <ruaald6@n-msk-macald6.raiffeisen.ru>
2019-09-16 01:17:24 +0300

    add About

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,7 +1,7 @@
 import Main from './Main';
 import Catalog from './Catalog';
 import Contacts from './Contacts';
-import About from './About';
+import About from './About/About';
 import NotFound from './NotFound';
 
 export { Main, Catalog, Contacts, About, NotFound };
