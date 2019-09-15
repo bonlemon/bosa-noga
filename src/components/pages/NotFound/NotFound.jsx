7504610f331376c9988cda9e9764620af1587fb3
AVILOV Dmitry <ruaald6@n-msk-macald6.raiffeisen.ru>
2019-09-16 01:27:24 +0300

    add Preloader component

--- src/components/pages/NotFound/NotFound.jsx
+++ src/components/pages/NotFound/NotFound.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 const NotFound = () => {
-    return <div className='NotFound'>NotFound</div>;
+    return (
+        <section className='top-sales'>
+            <h2 className='text-center'>Страница не найдена</h2>
+            <p>Извините, такая страница не найдена!</p>
+        </section>
+    );
 };
 export default NotFound;
