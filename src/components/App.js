fd7f8904dc77f44c9ffb24d689f34afac3385e71
AVILOV Dmitry <ruaald6@n-msk-macald6.raiffeisen.ru>
2019-09-17 01:12:30 +0300

    add rootSaga, Provider, Store, Constants

--- src/components/App.js
+++ src/components/App.js
@@ -3,25 +3,29 @@ import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
 import { Header, Content, Footer } from './sections';
 import { Main, Catalog, About, Contacts, NotFound } from './pages';
+import { Provider } from 'react-redux';
+import store from '../store';
 
 const App = () => {
     return (
-        <div className='App'>
+        <Provider store={store}>
             <BrowserRouter>
-                <Header />
-                <Content>
-                    <Switch>
-                        <Route path='/' exact component={Main} />
-                        <Route path='/catalog' exact component={Catalog} />
-                        <Route path='/about' exact component={About} />
-                        <Route path='/contacts' exact component={Contacts} />
-                        <Route path='/404' exact component={NotFound} />
-                        <Redirect to='/404' />
-                    </Switch>
-                </Content>
-                <Footer />
+                <div className='App'>
+                    <Header />
+                    <Content>
+                        <Switch>
+                            <Route path='/' exact component={Main} />
+                            <Route path='/catalog' exact component={Catalog} />
+                            <Route path='/about' exact component={About} />
+                            <Route path='/contacts' exact component={Contacts} />
+                            <Route path='/404' exact component={NotFound} />
+                            <Redirect to='/404' />
+                        </Switch>
+                    </Content>
+                    <Footer />
+                </div>
             </BrowserRouter>
-        </div>
+        </Provider>
     );
 };
 export default App;
