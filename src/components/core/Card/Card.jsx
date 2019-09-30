2c83d6c8a1669e203fa6eeafdbabd47d709f5139
ruaald6 <S4qtm81234>
2019-09-30 22:28:23 +0300

    add basket

--- src/components/core/Card/Card.jsx
+++ src/components/core/Card/Card.jsx
@@ -4,11 +4,17 @@ import { Link } from 'react-router-dom';
 import './Card.css';
 
 const Card = ({ id, image, title, price, className }) => {
+    const imgStyles = {
+        backgroundImage: `url(${image})`,
+        backgroundSize: 'contain',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+    };
     return (
         <div className='col-4'>
             <div className={`card ${className || ''}`}>
-                <div className='card-img'>
-                    <img src={image} className='card-img img-fluid' alt={title} />
+                <div className='card-img' style={imgStyles}>
+                    {/*<img src={image} className='card-img img-fluid' alt={title} />*/}
                 </div>
                 <div className='card-body'>
                     <p className='card-text'>{title}</p>
